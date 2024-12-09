bfa815ee69f2fac6262cbd360aa1803beb653a05
Arnaldo Marquisoslo <arnaldomj09@gmail.com>
2024-12-09 17:02:00 +0000

    Finalizado dimensao funcionarios e sua documentacao

--- models/comercial/intermediate/int_funcionarios_preparacao.sql
+++ models/comercial/intermediate/int_funcionarios_preparacao.sql
@@ -1,22 +1,24 @@
-with 
+with
     funcionarios as (
-    select *
-    from {{ref("stg_erp__funcionarios")}}
+        select *
+        from {{ ref('stg_erp__funcionarios') }}
     )
+
     , joined as (
-      select
-            funcionarios.pk_funcionario 
-          , funcionarios.nome_funcionario 
-          , gerentes.nome_funcionario as nome_gerente
-          , funcionarios.cargo_funcionario 
-          , funcionarios.dt_nascimento_funcionario 
-          , funcionarios.dt_contratacao 
-          , funcionarios.cidade_funcionario 
-          , funcionarios.regiao_funcionario 
-          , funcionarios.pais_funcionario 
-from funcionarios
-left join funcionarios as gerentes
-    on funcionarios.fk_gerente = gerentes.pk_funcionario
+        select
+            funcionarios.PK_FUNCIONARIO
+            , funcionarios.NOME_FUNCIONARIO
+            , gerentes.nome_funcionario as nome_gerente
+            , funcionarios.CARGO_FUNCIONARIO
+            , funcionarios.DT_NASCIMENTO_FUNCIONARIO
+            , funcionarios.DT_CONTRATACAO
+            , funcionarios.CIDADE_FUNCIONARIO
+            , funcionarios.REGIAO_FUNCIONARIO
+            , funcionarios.PAIS_FUNCIONARIO
+        from funcionarios
+        left join funcionarios as gerentes
+            on funcionarios.fk_gerente = gerentes.pk_funcionario
     )
+
 select *
 from joined
